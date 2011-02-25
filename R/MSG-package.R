92e077cf2f17f87d9a923286dce6ebe0500fee1e
Yihui Xie <xie@yihui.name>
2011-02-25 13:48:07 -0600

    tv earnings data

--- R/MSG-package.R
+++ R/MSG-package.R
@@ -329,3 +329,16 @@ NULL
 
 
 
+##' Top TV earners.
+##'
+##' The pay per episode for actors as well as other information.
+##' @name tvearn
+##' @docType data
+##' @keywords datasets
+##' @examples data(tvearn)
+##' plot(pay ~ rating, data=tvearn)
+##' if (require('ggplot2')) {
+##' qplot(pay,data=tvearn,geom='histogram',facets=gender~.,binwidth=20000)
+##' qplot(rating,pay,data=tvearn,geom=c('jitter','smooth'),color=type)
+##' }
+NULL
