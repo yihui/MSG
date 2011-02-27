818b87fd1cc243a78c0b812f3179eed90fd33ae5
Yihui Xie <xie@yihui.name>
2011-02-27 03:02:36 -0600

    added examples to gov.cn.pct

--- R/MSG-package.R
+++ R/MSG-package.R
@@ -162,7 +162,22 @@ NULL
 ##' @examples
 ##'
 ##' data(gov.cn.pct)
-##'
+##' pct.lowess = function(cond) {
+##'     with(gov.cn.pct, {
+##' plot(count ~ percentage, pch = ifelse(cond, 4, 20), col = rgb(0:1,
+##'         0, 0, c(0.04, .5))[cond + 1], log = "y")
+##'     lines(lowess(gov.cn.pct[cond, 1:2], f = 1/3), col = 2, lwd = 2)
+##'     lines(lowess(gov.cn.pct[!cond, 1:2], f = 1/3), col = 1, lwd = 2)
+##' })
+##' }
+##' par(mar = c(3.5, 3.5, 1, 0.2), mfrow = c(2, 2))
+##' with(gov.cn.pct, {
+##'     plot(percentage, count, type = "l", panel.first = grid())
+##'     plot(percentage, count, type = "l", xlim = c(10, 11), panel.first = grid())
+##'     pct.lowess(round0)
+##'     pct.lowess(round1)
+##' })
+##' if(interactive()){
 ##' devAskNewPage(ask = TRUE)
 ##'
 ##' with(gov.cn.pct, {
@@ -192,6 +207,7 @@ NULL
 ##'     lines(lowess(gov.cn.pct[!round1, 1:2], f = 1/3), col = "black",
 ##'         lwd = 2)
 ##' })
+##' }
 ##'
 NULL
 
