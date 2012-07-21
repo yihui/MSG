b54e889e57ccc4ba97ce5eecf6904235d63caca7
Yihui Xie <xie@yihui.name>
2012-07-21 15:38:14 -0400

    ##' -> #' and 2 spaces indent

--- R/color.R
+++ R/color.R
@@ -1,42 +1,43 @@
-##' Generate colors from a vector
-##'
-##' This functions generates a color vector from an input vector,
-##' which can be of the class numeric or factor.
-##' @rdname vec2col
-##' @param vec the numeric or factor vector
-##' @param n the number of colors to be generated from the palette
-##' @param name the name of the palette
-##' @return a vector of colors corresponding to the input vector
-##' @export
-##' @author Yihui Xie <\url{http://yihui.name}>
-##' @examples
-##' ## convert factor to colors
-##' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Species), pch = 19))
-##'
-##' # another palette
-##' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Species, name = 'Dark2'),
-##' pch = 19))
-##'
-##' ## turn numeric values to colors
-##' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Petal.Width), pch = 19))
+#' Generate colors from a vector
+#'
+#' This functions generates a color vector from an input vector, which can be of
+#' the class numeric or factor.
+#' @rdname vec2col
+#' @param vec the numeric or factor vector
+#' @param n the number of colors to be generated from the palette
+#' @param name the name of the palette
+#' @return a vector of colors corresponding to the input vector
+#' @export
+#' @author Yihui Xie <\url{http://yihui.name}>
+#' @examples
+#' ## convert factor to colors
+#' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Species), pch = 19))
+#'
+#' # another palette
+#' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Species, name = 'Dark2'),
+#' pch = 19))
+#'
+#' ## turn numeric values to colors
+#' with(iris, plot(Petal.Length, Petal.Width, col = vec2col(Petal.Width), pch = 19))
 vec2col = function(vec, n, name) {
-    UseMethod('vec2col')
+  UseMethod('vec2col')
 }
-##' @method vec2col default
-##' @rdname vec2col
-##' @S3method vec2col default
+#' @method vec2col default
+#' @rdname vec2col
+#' @S3method vec2col default
+#' @importFrom RColorBrewer brewer.pal
 vec2col.default = function(vec, n, name) {
-    if (missing(n)) n = 9
-    if (missing(name)) name = 'Blues'
-    idx = findInterval(vec, seq(min(vec, na.rm = TRUE), max(vec, na.rm = TRUE),
-                                length.out = n))
-    brewer.pal(n, name)[idx]
+  if (missing(n)) n = 9
+  if (missing(name)) name = 'Blues'
+  idx = findInterval(vec, seq(min(vec, na.rm = TRUE), max(vec, na.rm = TRUE),
+                              length.out = n))
+  brewer.pal(n, name)[idx]
 }
-##' @method vec2col factor
-##' @rdname vec2col
-##' @S3method vec2col factor
+#' @method vec2col factor
+#' @rdname vec2col
+#' @S3method vec2col factor
 vec2col.factor = function(vec, n, name) {
-    if (missing(n)) n = length(levels(vec))
-    if (missing(name)) name = 'Accent'
-    brewer.pal(n, name)[as.integer(vec)]
+  if (missing(n)) n = length(levels(vec))
+  if (missing(name)) name = 'Accent'
+  brewer.pal(n, name)[as.integer(vec)]
 }
