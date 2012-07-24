1e9dbe266db1ee942707f37073af2fe355c566d9
Lijia Yu <yulj2010@gmail.com>
2012-07-24 13:19:52 +0800

    Update demo/Napoleon.R

--- demo/Napoleon.R
+++ demo/Napoleon.R
@@ -4,36 +4,21 @@ markerdata <- read.csv(system.file("extdata", "Napoleon.csv", package = "MSG"))
 boundingbox <- qbbox(lon = markerdata[ , "lon"], lat = markerdata[ ,"lat"])
 NPMap <- GetMap.bbox(boundingbox$lonR, boundingbox$latR, destfile = "np.png", maptype = "map", zoom = 6, size = c(640, 370))
 
-lwd_m <- array(0.0001,dim=15)
-lwd_m[1:5] <- c(0.00015,0.00014,0.00013,0.00012,0.00011)
+lwd_m <- array(0.0001,dim=48)
+lwd_m[1:5] <- seq(0.00015,0.00011,by=-0.00001)
 
-# Group 1 Engage
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[1:2], lat=markerdata$lat[1:2], lwd = 0.00015 * markerdata$size[1], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), verbose=0)
-for (i in 2:15)
+color <- c("#8B000078","#00008B78")
+
+tmp <- c(16,35,41,43,46)
+c <- 1
+
+PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[1:2], lat=markerdata$lat[1:2], lwd = lwd_m[1]* markerdata$size[1], col = color[1], verbose=0)
+for (i in 2:48)
   {
-    PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[i:(i+1)], lat=markerdata$lat[i:(i+1)], lwd =  lwd_m[i]* markerdata$size[i], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
+    if (!(i%in%tmp)){
+      PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[i:(i+1)], lat=markerdata$lat[i:(i+1)], lwd =  lwd_m[i]* markerdata$size[i], col = color[c], add = TRUE, verbose=0)
+    }
+    else{
+      c<-which(tmp==i,arr.ind=TRUE)%%2+1
+    }
   }
-
-
-# Group 1 Lose
-for (j in 17:34)
-  {
-    PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[j:(j+1)], lat=markerdata$lat[j:(j+1)], lwd = 0.0001 * markerdata$size[j], col=rgb(0, 0, 139, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-  }
-
-# Group 2 Engage
-for (k in 36:40)
-  {
-    PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[k:(k+1)], lat=markerdata$lat[k:(k+1)], lwd = 0.0001 * markerdata$size[k], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-  }
-
-# Group 2 Lose
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[42:43], lat=markerdata$lat[42:43], lwd = 0.0001 * markerdata$size[42], col=rgb(0, 0, 139, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-
-# Group 3 Engage
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[44:45], lat=markerdata$lat[44:45], lwd = 0.0001 * markerdata$size[44], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[45:46], lat=markerdata$lat[45:46], lwd = 0.0001 * markerdata$size[45], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-
-# Group 3 Lose
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[47:48], lat=markerdata$lat[47:48], lwd = 0.0001 * markerdata$size[47], col=rgb(0, 0, 139, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
-PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[48:49], lat=markerdata$lat[48:49], lwd = 0.0001 * markerdata$size[48], col=rgb(0, 0, 139, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
