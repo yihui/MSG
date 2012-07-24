a12bbafd5005e7e9fa03b4519bd2d44b56ea745d
Lijia Yu <yulj2010@gmail.com>
2012-07-24 10:31:39 +0800

    Update demo/Napoleon.R

--- demo/Napoleon.R
+++ demo/Napoleon.R
@@ -6,17 +6,17 @@ NPMap <- GetMap.bbox(boundingbox$lonR, boundingbox$latR, destfile = "np.png", ma
 
 # Group 1 Engage
 PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[1:2], lat=markerdata$lat[1:2], lwd = 0.00015 * markerdata$size[1], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), verbose=0)
-for i in 2:15{
+for (i in 2:15){
   PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[i:i+1], lat=markerdata$lat[i:i+1], lwd = 0.00015 * markerdata$size[i], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
 }
 
 # Group 1 Lose
-for j in i+1:34{
+for (j in i+1:34){
   PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[j:j+1], lat=markerdata$lat[j:j+1], lwd = 0.0001 * markerdata$size[j], col=rgb(0, 0, 139, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
 }
 
 # Group 2 Engage
-for k in j+1:40{
+for (k in j+1:40){
   PlotOnStaticMap(NPMap, FUN = lines, lon=markerdata$lon[k:k+1], lat=markerdata$lat[k:k+1], lwd = 0.0001 * markerdata$size[k], col = rgb(139, 0, 0, alpha = 120, maxColorValue = 255), add = TRUE, verbose=0)
 }
 
