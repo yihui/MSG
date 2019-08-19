0af1a513c5832886c87daa85b6aaeb6e17124794
Yihui Xie <xie@yihui.name>
2019-08-19 09:30:18 -0500

    install libglu1-mesa-dev for rgl

--- demo/pointArts.R
+++ demo/pointArts.R
@@ -5,9 +5,7 @@ for (n in 41:200) {
     size = c(replicate(n, 1/rbeta(2, 1.5, 4)))
     center = t(replicate(n, runif(2)))
     center = center[rep(1:n, each = 2), ]
-    color = apply(replicate(2 * n, sample(c(0:9, LETTERS[1:6]),
-        8, replace = TRUE)), 2, function(x) sprintf("#%s", paste(x,
-        collapse = "")))
+    color = apply(replicate(2 * n, sample(c(0:9, LETTERS[1:6]), 8, replace = TRUE)), 2, function(x) sprintf("#%s", paste(x,collapse = "")))
     points(center, cex = size, pch = rep(20:21, n), col = color)
 	box()
     # text(0.5, 0.5, n)
