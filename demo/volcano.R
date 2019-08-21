4ea93b4b2c35486172bec98e5103a9c1e291ed36
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-08-21 23:10:37 +0800

    更新统计词话数据案例的代码和数据、重新格式化代码 (#31)

--- demo/volcano.R
+++ demo/volcano.R
@@ -1,5 +1,16 @@
-par(mar=c(4,4,2,2),cex.main=1)
+par(mar = c(4, 4, 2, 2), cex.main = 1)
 x = 10 * 1:nrow(volcano)
 y = 10 * 1:ncol(volcano)
-filled.contour(x, y, volcano, color = terrain.colors,plot.title = title(main = "The Topography of Maunga Whau",xlab = "Meters North", ylab = "Meters West"),plot.axes = {axis(1, seq(100, 800, by = 100))
-	axis(2, seq(100, 600, by = 100))}, key.title =title(main = "Height\n(meters)"), key.axes = axis(4,seq(90, 190, by = 10)))
+filled.contour(x, y, volcano,
+  color = terrain.colors,
+  plot.title = title(
+    main = "The Topography of Maunga Whau",
+    xlab = "Meters North", ylab = "Meters West"
+  ),
+  plot.axes = {
+    axis(1, seq(100, 800, by = 100))
+    axis(2, seq(100, 600, by = 100))
+  },
+  key.title = title(main = "Height\n(meters)"),
+  key.axes = axis(4, seq(90, 190, by = 10))
+)
