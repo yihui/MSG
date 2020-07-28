3672dbc7529e668faeb818b7c5b5f702d08028b8
Peng Zhao <baydap@gmail.com>
2020-07-28 20:20:46 +0800

    新增 print_plot 参数。箭头换等号。

--- R/msg.R
+++ R/msg.R
@@ -1,7 +1,8 @@
 #' Plot a graph with a pre-installed R script
 #'
 #' @param fig The figure number or the R script name, which is given in the book.
-#' @param show_code Logical.
+#' @param show_code Logical. TRUE means the codes are shown in the console.
+#' @param print_plot Logical. TRUE means the graph is printed.
 #' @importFrom utils read.csv
 #'
 #' @return A graph and the source code
@@ -10,10 +11,10 @@
 #' @examples
 #' # msg(3.6)
 #' # msg("ChinaPop")
-msg <- function(fig = 3.6, show_code = TRUE){
-  graphnr <- read.csv(system.file("extdata", "graphnr.csv", package = "MSG"))
-  if(is.numeric(fig)) fig <- graphnr$graph[graphnr$nr == fig]
-  rfile <- system.file("examples", paste0(fig, ".R"), package = "MSG")
-  source(rfile, encoding = "UTF-8")
+msg = function(fig = 3.6, show_code = TRUE, print_plot = TRUE){
+  graphnr = read.csv(system.file("extdata", "graphnr.csv", package = "MSG"))
+  if(is.numeric(fig)) fig = graphnr$graph[graphnr$nr == fig]
+  rfile = system.file("examples", paste0(fig, ".R"), package = "MSG")
+  if(print_plot) source(rfile, encoding = "UTF-8")
   if(show_code) cat(readLines(rfile, encoding = "UTF-8"), sep = '\n')
 }
