e37a7a8fe53f23141e787d1ae7e0cfaabe2208c0
pzhaonet <pzhao@pzhao.net>
2020-08-06 11:54:00 +0800

    代码换行

--- inst/examples/tvearn.R
+++ inst/examples/tvearn.R
@@ -10,7 +10,7 @@ dh2 = ggplot(aes(x = rating, y = pay, color = type), data = tvearn) +
   geom_smooth(method = "loess") +
   scale_y_continuous(
     labels = scales::unit_format(unit = "w", scale = 1e-4),
-    breaks = seq(0, 125, 15) * 10^4
-  ) +
-  theme(legend.background = element_blank(), legend.position = c(0.1, 0.9))
+    breaks = seq(0, 125, 15) * 10^4) +
+  theme(legend.background = element_blank(),
+        legend.position = c(0.1, 0.9))
 print(dh1 / dh2)
