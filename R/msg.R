7e7274056fc17a308b8c48be45b35ba18ef0964e
pzhaonet <pzhao@pzhao.net>
2020-08-06 10:56:38 +0800

    msg() 新增 filter 参数

--- R/msg.R
+++ R/msg.R
@@ -2,7 +2,9 @@
 #'
 #' @param fig The figure number or the R script name, which is given in the book.
 #' @param show_code Logical. TRUE means the codes are shown in the console.
+#' @param filter Integer. The line numbers indicating which lines in the code are displayed (when positive) or hidden (when negative).
 #' @param print_plot Logical. TRUE means the graph is printed.
+#'
 #' @importFrom utils read.csv
 #'
 #' @return A graph and the source code
@@ -11,10 +13,12 @@
 #' @examples
 #' # msg(3.6)
 #' # msg("ChinaPop")
-msg = function(fig = 3.6, show_code = TRUE, print_plot = TRUE){
+msg = function(fig = 3.6, show_code = TRUE, print_plot = TRUE, filter = 0){
   graphnr = read.csv(system.file("extdata", "graphnr.csv", package = "MSG"))
-  if(is.numeric(fig)) fig = graphnr$graph[graphnr$nr == fig]
+  if (is.numeric(fig)) fig = graphnr$graph[graphnr$nr == fig]
   rfile = system.file("examples", paste0(fig, ".R"), package = "MSG")
-  if(print_plot) source(rfile, encoding = "UTF-8")
-  if(show_code) cat(readLines(rfile, encoding = "UTF-8"), sep = '\n')
+  if (print_plot) source(rfile, encoding = "UTF-8")
+  if (show_code) {
+    if (!0 %in% filter) rfile = rfile[filter]
+    cat(readLines(rfile, encoding = "UTF-8"), sep = '\n')}
 }
