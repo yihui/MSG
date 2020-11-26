43c582506eab60299ab72f554a8b37743bf40662
pzhaonet <pzhao@pzhao.net>
2020-11-26 11:35:11 +0800

    删除多余的 tikz 示例

--- inst/examples/pay-rating-points-tikz.R
+++
@@ -1,7 +0,0 @@
-# 演员收入与电视剧评分的空心和实心散点图
-par(mfrow = c(1, 2))
-data("tvearn", package = "MSG")
-plot(pay/10^4 ~ rating, data = tvearn, # 默认为空心点
-     xlab = "评分", ylab = "薪酬 ($10^4$)")
-plot(pay/10^4 ~ rating, data = tvearn, pch = 19, # 改为实心点
-     xlab = "评分", ylab = "薪酬 ($10^4$)")
--- inst/examples/pay-rating-points.R
+++ inst/examples/pay-rating-points.R
@@ -1,7 +1,7 @@
 # 演员收入与电视剧评分的空心和实心散点图
 par(mfrow = c(1, 2))
 data("tvearn", package = "MSG")
-plot(pay/10^4 ~ rating, data = tvearn,
-     ylab = "pay (10^4)") # 默认为空心点
-plot(pay/10^4 ~ rating, data = tvearn, pch = 19,
-     ylab = "pay (10^4)") # 改为实心点
+plot(pay/10^4 ~ rating, data = tvearn, # 默认为空心点
+     xlab = "评分", ylab = "薪酬 (万)")
+plot(pay/10^4 ~ rating, data = tvearn, pch = 19, # 改为实心点
+     xlab = "评分", ylab = "薪酬 (万)")
